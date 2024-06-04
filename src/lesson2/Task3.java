364cf7b98cb89d0cb39e5fbfa7faabf99c05c6ba
Raman <rdubouski@gmail.com>
2024-06-05 01:02:04 +0300

    Lesson 2, Task 3 complete

---
+++ src/lesson2/Task3.java
@@ -0,0 +1,26 @@
+package lesson2;
+
+//Вывести true если в числе нет цифр дубликатов
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int number = 12345678;
+        System.out.println("number = " + number);
+        int number2 = number / 10;
+        int check = number % 10;
+        boolean checkunique = true;
+        while (number != 0) {
+            while (number2 != 0){
+                if (check == number2 % 10) {
+                    checkunique = false;
+                    System.out.println(checkunique);
+                    return;
+                }
+                number2 /= 10;
+            }
+            number /= 10;
+        }
+        System.out.println(checkunique);
+    }
+}
