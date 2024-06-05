016c74c15171b10fc608768351ee0e5479d9c902
Raman <rdubouski@gmail.com>
2024-06-06 01:10:18 +0300

    Lesson 2, Task 4 without first digit

--- src/lesson2/Task4.java
+++ src/lesson2/Task4.java
@@ -5,6 +5,30 @@ package lesson2;
 public class Task4 {
 
     public static void main(String[] args) {
-
+        long number = 912354568789L;
+        System.out.println("number = " + number);
+        long number2 = number / 10;
+        long check = number % 10;
+        long sequence = 0;
+        while (number != 0) {
+            while (number2 != 0){
+                if ((check - 1) != number2 % 10) {
+                    if (sequence != 0) {
+                        sequence = sequence * 10 + check;
+                        System.out.println("sequence = " + sequence);
+                        number = number2;
+                    }
+                    break;
+                } else {
+                    sequence = sequence * 10 + check;
+                }
+                check = number2 % 10;
+                number2 /= 10;
+            }
+            sequence = 0;
+            number /= 10;
+            number2 = number / 10;
+            check = number % 10;
+        }
     }
 }
