2fa41fd6e9d2733670850c2c30a48450ec330de8
Raman <rdubouski@gmail.com>
2024-06-06 22:36:35 +0300

    Lesson 2, Task 4 complete

--- src/lesson2/Task4.java
+++ src/lesson2/Task4.java
@@ -5,30 +5,22 @@ package lesson2;
 public class Task4 {
 
     public static void main(String[] args) {
-        long number = 912354568789L;
+        long number = 23572348357L;
         System.out.println("number = " + number);
-        long number2 = number / 10;
-        long check = number % 10;
         long sequence = 0;
-        while (number != 0) {
-            while (number2 != 0){
-                if ((check - 1) != number2 % 10) {
-                    if (sequence != 0) {
-                        sequence = sequence * 10 + check;
-                        System.out.println("sequence = " + sequence);
-                        number = number2;
-                    }
-                    break;
-                } else {
-                    sequence = sequence * 10 + check;
+        while (number != 0){
+            if (number % 10 + 1 == sequence % 10) {
+                sequence = sequence * 10 + number % 10;
+            } else {
+                if (sequence / 10 > 0) {
+                    System.out.println("sequence = " + sequence);
                 }
-                check = number2 % 10;
-                number2 /= 10;
+                sequence = number % 10;
             }
-            sequence = 0;
             number /= 10;
-            number2 = number / 10;
-            check = number % 10;
+        }
+        if (sequence / 10 > 0) {
+            System.out.println("sequence = " + sequence);
         }
     }
 }
